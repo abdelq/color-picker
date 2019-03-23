2234e57064f2ddc9a05f190ce2e86d6d516bcf01
debouvie <47230753+debouvie@users.noreply.github.com>
2019-03-23 00:26:22 -0400

    Update ColoredSeekBar.java

--- ColorPicker/app/src/main/java/com/example/colorpicker/ColoredSeekBar.java
+++ ColorPicker/app/src/main/java/com/example/colorpicker/ColoredSeekBar.java
@@ -8,11 +8,15 @@ import android.support.v7.widget.AppCompatSeekBar;
 import android.util.AttributeSet;
 import android.widget.SeekBar;
 
+import com.example.colorpicker.Views.AreaPicker;
+
 public class ColoredSeekBar extends AppCompatSeekBar {
     private final static int MAX_RGB_VALUE = 255;
     private int isRed, isGreen, isBlue;
     private int r, g, b;
     private ColorPickerDialog colorPickerDialog;
+    private HSeekBar hBar;
+    private AreaPicker area;
     GradientDrawable gd;
 
     public ColoredSeekBar(Context context) {
@@ -38,8 +42,11 @@ public class ColoredSeekBar extends AppCompatSeekBar {
         gd.setStroke(1, Color.BLACK);
     }
 
-    public void setColorPickerDialog(ColorPickerDialog colorPickerDialog){
+    public void setElements(ColorPickerDialog colorPickerDialog,
+                            HSeekBar hBar, AreaPicker area){
         this.colorPickerDialog = colorPickerDialog;
+        this.hBar = hBar;
+        this.area = area;
     }
 
     OnSeekBarChangeListener listener = new OnSeekBarChangeListener() {
@@ -49,8 +56,17 @@ public class ColoredSeekBar extends AppCompatSeekBar {
             g = isGreen*progress+(1-isGreen)*g;
             b = isBlue*progress+(1-isBlue)*b;
 
+            float h = colorPickerDialog.getH();
+            float s = colorPickerDialog.getS();
+            float v = colorPickerDialog.getV();
+
             colorPickerDialog.setColor(Color.rgb(r, g, b));
             colorPickerDialog.updateColors();
+
+            if(fromUser) {
+                updateHBar(h);
+                updateArea(s,v);
+            }
         }
 
         @Override
@@ -59,6 +75,17 @@ public class ColoredSeekBar extends AppCompatSeekBar {
         @Override
         public void onStopTrackingTouch(SeekBar seekBar) { }
     };
+
+    // update la valeur de la seekBar qui représente h
+    void updateHBar(float h) {
+        hBar.setProgress((int)h);
+    }
+
+    // update les valeurs de x et y dans AreaPicker
+    void updateArea(float s, float v) {
+        area.setPickedX(s/100);
+        area.setPickedY((100-v)/100);
+    }
 
     public void setColorSeekBar(int isRed, int isGreen, int isBlue){
         this.isRed = isRed;
