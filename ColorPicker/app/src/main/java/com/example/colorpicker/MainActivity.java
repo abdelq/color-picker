200881800a5e9188b14b8467c30b7d682ba7b249
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-03-29 13:03:50 -0400

    Add missing checkers background for picked color

--- ColorPicker/app/src/main/java/com/example/colorpicker/MainActivity.java
+++ ColorPicker/app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.colorpicker;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.InsetDrawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 class MainActivity extends AppCompatActivity {
     @Override
@@ -9,9 +15,17 @@ class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View pickedColor = findViewById(R.id.picked_color);
+        pickedColor.setBackground(new LayerDrawable(new Drawable[]{
+                new InsetDrawable(getBaseContext().getDrawable(R.drawable.checker), 0),
+                new ColorDrawable(Color.BLACK)
+        }));
+
         ColorPickerDialog colorPickerDialog = new ColorPickerDialog(this);
-        colorPickerDialog.setOnColorPickedListener((dialog, color) ->
-                findViewById(R.id.picked_color).setBackgroundColor(color));
+        colorPickerDialog.setOnColorPickedListener((dialog, color) -> {
+            LayerDrawable layer = (LayerDrawable) pickedColor.getBackground();
+            ((ColorDrawable) layer.getDrawable(1)).setColor(color);
+        });
         findViewById(R.id.button_pick).setOnClickListener(view -> colorPickerDialog.show());
     }
 }
